31d8ebb0a12ea4d030df952c3f66de14c2ab529e
Manish <mkmk6499@gmail.com>
2024-11-07 02:21:29 +0530

    Foreground Service

--- app/src/main/java/com/android/practiceapplication/Service/ServiceLearning.kt
+++ app/src/main/java/com/android/practiceapplication/Service/ServiceLearning.kt
@@ -1,16 +1,21 @@
 package com.android.practiceapplication.Service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import kotlin.math.log
+import androidx.core.app.NotificationCompat
+import com.android.practiceapplication.R
 
 class ServiceLearning : Service() {
     private  val TAG = "Service Class"
-
+    companion object {
+        const val NOTIFICATION_ID = 1
+    }
     override fun onCreate() {
         super.onCreate()
         application
@@ -25,6 +30,8 @@ class ServiceLearning : Service() {
 
     override fun onDestroy() {
         Log.d(TAG, "onDestroy: ")
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
         super.onDestroy()
     }
 
@@ -45,7 +52,31 @@ class ServiceLearning : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "onStartCommand: ")
+        startForegroundService()
         return super.onStartCommand(intent, flags, startId)
+    }
+    private fun startForegroundService() {
+        val notification = createNotification()
+        //here we are creating foreground service
+        startForeground(NOTIFICATION_ID, notification)
+    }
+    private fun createNotification(): Notification {
+        val notificationChannelId = "MY_SERVICE_CHANNEL"
+
+            val channel = NotificationChannel(
+                notificationChannelId,
+                "My Foreground Service",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+
+
+        return NotificationCompat.Builder(this, notificationChannelId)
+            .setContentTitle("Service Running")
+            .setContentText("Your service is running in the foreground")
+            .setSmallIcon(R.drawable.ic_service_icon) // Replace with your icon
+            .build()
     }
 
 
@@ -53,6 +84,11 @@ class ServiceLearning : Service() {
         fun getService() : ServiceLearning{
             return this@ServiceLearning
         }
+    }
+
+    override fun onTimeout(startId: Int) {
+        Log.d(TAG, "onTimeout: $startId")
+        super.onTimeout(startId)
     }
 
 }
